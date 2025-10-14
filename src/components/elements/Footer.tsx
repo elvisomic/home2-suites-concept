21e0ac2eb1d46f68fdfe8197230d746687735afd
Elvis Omić <elvis08.eo@gmail.com>
2025-10-14 23:57:40 +0200

    Footer and navbar image location fix

--- src/components/elements/Footer.tsx
+++ src/components/elements/Footer.tsx
@@ -5,22 +5,22 @@ export const Footer = () => {
 
             <div className="h-full md:grid md:grid-cols-[50%_50%] items-center md:px-4 lg:px-20 py-8 justify-center">
                 <div className="flex justify-center items-center md:h-34 md:w-64 2xl:h-54 2xl:w-104">
-                    <img src="home2-suites-concept/assets/Home2.png" className="2xl:h-40 2xl:w-80 md:h-30 md:w-60 h-30 w-60" />
+                    <img src="/home2-suites-concept/assets/Home2.png" className="2xl:h-40 2xl:w-80 md:h-30 md:w-60 h-30 w-60" />
                 </div>
                 <div className="h-full p-7 md:p-14 flex items-center justify-center">
                     <div className="flex flex-col gap-y-8">
                         <div className="flex flex-row gap-x-2">
-                            <img src="home2-suites-concept/assets/svg/walkingIcon.svg" className=" h-6" />
+                            <img src="/home2-suites-concept/assets/svg/walkingIcon.svg" className=" h-6" />
                             <h1>150 W 48th Street, New York, New York, 10017, USA</h1>
                         </div>
 
                         <div className="flex flex-row gap-x-20">
                             <div className="flex flex-row gap-x-2">
-                                <img src="home2-suites-concept/assets/svg/walkingIcon.svg" className="h-6" />
+                                <img src="/home2-suites-concept/assets/svg/walkingIcon.svg" className="h-6" />
                                 <h1>+1-877-6HOME02</h1>
                             </div>
                             <div className="flex flex-row gap-x-2">
-                                <img src="home2-suites-concept/assets/svg/walkingIcon.svg" className="h-6" />
+                                <img src="/home2-suites-concept/assets/svg/walkingIcon.svg" className="h-6" />
                                 <h1>+1-877-6HOME02</h1>
                             </div>
                         </div>
@@ -30,9 +30,9 @@ export const Footer = () => {
                                 <h1>Social Media</h1>
                             </div>
                             <div className="flex flex-row items-center gap-x-4">
-                                <img src="home2-suites-concept/assets/logo/facebook.svg" className="h-14" />
-                                <img src="home2-suites-concept/assets/logo/x.png" className="h-10" />
-                                <img src="home2-suites-concept/assets/logo/instagram.svg" className="ml-2 h-11" />
+                                <img src="/home2-suites-concept/assets/logo/facebook.svg" className="h-14" />
+                                <img src="/home2-suites-concept/assets/logo/x.png" className="h-10" />
+                                <img src="/home2-suites-concept/assets/logo/instagram.svg" className="ml-2 h-11" />
                             </div>
                         </div>
 
