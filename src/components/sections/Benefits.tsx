4f9a6947185120d996ee53d06a13374fa4eb372b
Elvis Omić <elvis08.eo@gmail.com>
2025-10-20 20:56:07 +0200

    Benefits section changes of images and text

--- src/components/sections/Benefits.tsx
+++ src/components/sections/Benefits.tsx
@@ -13,44 +13,47 @@ export const Benefits = () => {
                 <p className="flex 2xl:text-[20px]/7 lg:mx-46 xl:mx-86 text-center font-light">Hilton Honors Member Benefits give guests exclusive savings, the ability to earn rewards, and access to convenient digital tools that make every stay easier and more rewarding.</p>
             </div>
 
-            <div className="h-auto lg:grid lg:grid-cols-3 lg:grid-rows-2 lg:gap-x-3 lg:gap-y-3 lg:mt-35">
+            <div className="h-auto lg:grid lg:grid-cols-3 lg:grid-rows-2 lg:gap-x-4 lg:gap-y-4 lg:mt-35">
 
-                <div className="p-5 overflow-y-hidden rounded-lg row-span-2 h-full bg-gray-300 grid grid-rows-[80%_20%]">
-                    <div className="bg-white h-full w-full flex justify-center">
-                        <img src="/home2-suites-concept/assets/bentoGrid/Coins-rafiki.png" className="h-120 object-cover" />
+                <div className="p-3 overflow-y-hidden rounded-lg row-span-2 h-full bg-[#F0F2F5] border-1 border-[#E0E0E0] shadow-xl grid grid-rows-[70%_30%] xl:grid-rows-[75%_25%] 2xl:grid-rows-[75%_25%]">
+                    <div className="h-full w-full flex justify-center items-end">
+                        <img src="/home2-suites-concept/assets/bentoGrid/points.png" className="xl:h-80 2xl:h-100 object-cover" />
                     </div>
                     <div className="2xl:max-w-[400px]">
-                        <h1 className="text-[24px]/12 font-semibold">Points and Perks</h1>
-                        <p className="opacity-90 break-words">Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae eum aliquam, rem illum quas laborum vitae corporis similique provident</p>
+                        <hr className="border-black/70" />
+                        <h1 className="text-[20px] xl:text-[24px]/10 font-semibold">Points and Perks</h1>
+                        <p className="opacity-90 break-words">Every stay brings you closer to more rewards. Earn Hilton Honors Points for every night, and enjoy exclusive benefits like free nights, room upgrades, and premium experiences as you move up in membership tiers.</p>
                     </div>
                 </div>
-                <div className="p-5 rounded-lg col-span-2 lg:h-105 xl:h-90 2xl:h-80 bg-gray-300 grid grid-cols-[70%_30%]">
-                    <div className="bg-white h-full w-full flex justify-center">
-                        <img src="/home2-suites-concept/assets/bentoGrid/Hotel Booking-cuate.png" className="h-70" />
+                <div className="p-3 rounded-lg col-span-2 lg:h-90 xl:h-90 2xl:h-90 bg-[#F0F2F5] border-1 border-[#E0E0E0] shadow-xl grid grid-cols-[60%_40%] 2xl:grid-cols-[70%_30%] ">
+                    <div className="h-full w-full flex justify-center items-center overflow-hidden">
+                        <img src="/home2-suites-concept/assets/bentoGrid/Discounts.png" className="h-60 w-90 2xl:h-70 2xl:w-100" />
                     </div>
                     <div className="mx-4 2xl:max-w-[400px]">
-                        <h1 className="text-[24px]/12 font-semibold">Points and Perks</h1>
-                        <p className="opacity-90 break-words">Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae eum aliquam, rem illum quas laborum vitae corporis similique provident</p>
+                        <h1 className="text-[20px] xl:text-[24px]/10 font-semibold">Discount Rates</h1>
+                        <p className="opacity-90 break-words">Loyalty pays off - unlock special discounted rates after 10, 20, and 30 days of stay. The longer you stay with Hilton, the more you save. Check your Hilton Honors benefits to see what's waiting for you.</p>
                     </div>
 
                 </div>
-                <div className="p-5 rounded-lg lg:h-105 xl:h-90 2xl:h-80 bg-gray-300 grid grid-rows-[60%_40%]">
-                    <div className="bg-white h-full w-full flex  px-4 overflow-y-hidden">
-                        <img src="/home2-suites-concept/assets/bentoGrid/Date picker-amico.png" className="h-40" />
+                <div className="p-3 rounded-lg lg:h-90 xl:h-90 2xl:h-90 bg-[#F0F2F5] border-1 border-[#E0E0E0] shadow-xl grid grid-rows-[50%_50%] 2xl:grid-rows-[60%_40%]">
+                    <div className="h-full w-full flex px-4 justify-center items-center overflow-y-hidden">
+                        <img src="/home2-suites-concept/assets/bentoGrid/digitalKey.png" className="h-50 w-70" />
                     </div>
                     <div className="2xl:max-w-[400px]">
-                        <h1 className="text-[24px]/12 font-semibold">Points and Perks</h1>
-                        <p className="opacity-90 break-words">Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae eum aliquam, rem illum quas laborum vitae corporis similique provident</p>
+                        <hr className="border-black/70" />
+                        <h1 className="text-[20px] xl:text-[24px]/10 font-semibold">Digital Key and Check-In</h1>
+                        <p className="opacity-90 break-words">Save time and skip the front desk with Digital Check-In. Use your phone to check in, choose your room, and unlock your door - all through the Hilton Honors app for a seamless arrival experience.</p>
                     </div>
 
                 </div>
-                <div className="p-5 rounded-lg lg:h-105 xl:h-90 2xl:h-80 bg-gray-300 grid grid-rows-[60%_40%]">
-                    <div className="bg-white h-full w-full flex justify-center">
-                        <img src="/home2-suites-concept/assets/bentoGrid/room.png" className="h-40" />
+                <div className="p-3 rounded-lg lg:h-90 xl:h-90 2xl:h-90 bg-[#F0F2F5] border-1 border-[#E0E0E0] shadow-xl grid grid-rows-[50%_50%] 2xl:grid-rows-[60%_40%]">
+                    <div className="h-full w-full flex px-4 justify-center items-center overflow-y-hidden">
+                        <img src="/home2-suites-concept/assets/bentoGrid/roomView.png" className="h-50 w-70" />
                     </div>
                     <div className="2xl:max-w-[400px]">
-                        <h1 className="text-[24px]/12 font-semibold">Points and Perks</h1>
-                        <p className="opacity-90 break-words">Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae eum aliquam, rem illum quas laborum vitae corporis similique provident</p>
+                        <hr className="border-black/70" />
+                        <h1 className="text-[20px] xl:text-[24px]/10 font-semibold">Choose Your Room</h1>
+                        <p className="opacity-90 break-words">Enjoy the freedom to select the perfect room before you arrive. Whether you prefer a high floor, a specific view, or proximity to amenities, you're in control of your stay from the start.</p>
                     </div>
 
                 </div>
