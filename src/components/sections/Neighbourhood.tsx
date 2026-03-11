2b53c604a256dbbf27f7951ec09ea7da74beae06
Elvis Omić <elvis08.eo@gmail.com>
2026-03-11 15:10:32 +0100

    SEO Fixes V1

--- src/components/sections/Neighbourhood.tsx
+++ src/components/sections/Neighbourhood.tsx
@@ -8,25 +8,25 @@ export const Neighbourhood = () => {
                 <h1 className="flex text-[40px]/25 justify-center font-semibold">What's Nearby</h1>
                 <p className="flex text-[20px]/7 justify-center font-light">Conveniently located, our hotel is surrounded by a variety of attractions, dining options, and local landmarks. Guests can easily explore nearby shops, restaurants, and cultural highlights, making it simple to experience the best of the area during their stay.</p>
             </div>
-            <div className="relative lg:grid lg:grid-cols-3 lg:grid-rows-2 2xl:gap-y-[140px] 2xl:mt-25 mb-70 lg:gap-y-[100px] xl:gap-y-[120px] justify-items-center w-full 2xl:gap-x-35 xl:gap-x-48 lg:gap-x-10 ">
+            <div className="relative lg:grid lg:grid-cols-3 lg:grid-rows-2 2xl:gap-y-[140px] 2xl:mt-25 mb-70 lg:gap-y-[100px] xl:gap-y-[120px] justify-items-center w-full 2xl:gap-x-35 xl:gap-x-48 lg:gap-x-10">
 
-                <NeighbourhoodCard imageSrc="/home2-suites-concept/assets/NeighbourhoodSection/EmpireStateBuilding2.jpg" title="Empire State Building" description="Breathtaking views from New York's most iconic tower" buttonText="EXPLORE" />
+                <NeighbourhoodCard imageSrc="/home2-suites-concept/assets/NeighbourhoodSection/EmpireStateBuilding2.jpg" title="Empire State Building" description="Breathtaking views from New York's most iconic tower" buttonText="EXPLORE" altText="Image showing breathtaking view of Empire State Building" />
 
-                <NeighbourhoodCard imageSrc="/home2-suites-concept/assets/NeighbourhoodSection/grandCentral2.jpg" title="Grand Central Terminal" description="A stunning landmark where history meets hustle" buttonText="EXPLORE" offset="translate-y-[100px]" />
+                <NeighbourhoodCard imageSrc="/home2-suites-concept/assets/NeighbourhoodSection/grandCentral2.jpg" title="Grand Central Terminal" description="A stunning landmark where history meets hustle" buttonText="EXPLORE" offset="translate-y-[100px]" altText="Image showing Grand Central Terminal" />
 
-                <NeighbourhoodCard imageSrc="/home2-suites-concept/assets/NeighbourhoodSection/edgeTest.avif" title="The Edge" description="Sky-high thrills on Manhattan's boldest observation deck" buttonText="EXPLORE" />
+                <NeighbourhoodCard imageSrc="/home2-suites-concept/assets/NeighbourhoodSection/edgeTest.avif" title="The Edge" description="Sky-high thrills on Manhattan's boldest observation deck" buttonText="EXPLORE" altText="Image showing The Edge observation deck" />
 
-                <NeighbourhoodCard imageSrc="/home2-suites-concept/assets/NeighbourhoodSection/vesselTest.jpg" title="Vessel" description="Climbable art piece with unforgettable city views" buttonText="EXPLORE" />
+                <NeighbourhoodCard imageSrc="/home2-suites-concept/assets/NeighbourhoodSection/vesselTest.jpg" title="Vessel" description="Climbable art piece with unforgettable city views" buttonText="EXPLORE" altText="Image showing The Vessel" />
 
-                <NeighbourhoodCard imageSrc="/home2-suites-concept/assets/NeighbourhoodSection/timesSquare2.jpg" title="Times Square" description="Bright lights and energy at the heart of New York" buttonText="EXPLORE" offset="translate-y-[100px]" />
+                <NeighbourhoodCard imageSrc="/home2-suites-concept/assets/NeighbourhoodSection/timesSquare2.jpg" title="Times Square" description="Bright lights and energy at the heart of New York" buttonText="EXPLORE" offset="translate-y-[100px]" altText="Image showing stunning view of Times Square" />
 
-                <NeighbourhoodCard imageSrc="/home2-suites-concept/assets/NeighbourhoodSection/museum2.jpg" title="American Museum of Natural History" description="Fascinating exhibits and discoveries for all ages" buttonText="EXPLORE" />
+                <NeighbourhoodCard imageSrc="/home2-suites-concept/assets/NeighbourhoodSection/museum2.jpg" title="American Museum of Natural History" description="Fascinating exhibits and discoveries for all ages" buttonText="EXPLORE" altText="Image showing Museum of Natural History" />
 
-                <NeighbourhoodCard imageSrc="/home2-suites-concept/assets/NeighbourhoodSection/centralPark2.jpg" title="Central Park" description="Endless paths, lakes, and escapes in the city's backyard" buttonText="EXPLORE" />
+                <NeighbourhoodCard imageSrc="/home2-suites-concept/assets/NeighbourhoodSection/centralPark2.jpg" title="Central Park" description="Endless paths, lakes, and escapes in the city's backyard" buttonText="EXPLORE" altText="Image showing aerial view of Central Park" />
 
-                <NeighbourhoodCard imageSrc="/home2-suites-concept/assets/NeighbourhoodSection/Flatiron2.jpg" title="Flatiron Building" description="A uniquely shaped icon made for photos" buttonText="EXPLORE" offset="translate-y-[100px]" />
+                <NeighbourhoodCard imageSrc="/home2-suites-concept/assets/NeighbourhoodSection/Flatiron2.jpg" title="Flatiron Building" description="A uniquely shaped icon made for photos" buttonText="EXPLORE" offset="translate-y-[100px]" altText="Image showing Flat Iron Building" />
 
-                <NeighbourhoodCard imageSrc="/home2-suites-concept/assets/NeighbourhoodSection/BroadwayC.jpg" title="Broadway" description="World-class performances on New York’s famous stage" buttonText="EXPLORE" />
+                <NeighbourhoodCard imageSrc="/home2-suites-concept/assets/NeighbourhoodSection/BroadwayC.jpg" title="Broadway" description="World-class performances on New York's famous stage" buttonText="EXPLORE" altText="Image showing Broadway street" />
 
             </div>
 
--- src/components/sections/Photos.tsx
+++ src/components/sections/Photos.tsx
@@ -1,5 +1,5 @@
-const Slika = ({ src, cName }: { src:string, cName:string }) => {
-    return <img src={src} loading="lazy" className={`object-cover w-full transform transition-transform duration-400 hover:scale-120 cursor-pointer ${cName} h-full`} />
+const Slika = ({ src, cName, altText }: { src:string, cName:string, altText:string }) => {
+    return <img src={src} loading="lazy" className={`object-cover w-full transform transition-transform duration-400 hover:scale-120 cursor-pointer ${cName} h-full`} alt={`${altText}`} />
 }
 
 const Z = ({ children, cols, rows, h, cName }: { children:React.ReactNode, cols:string, rows:string, h:string, cName?:string }) => {
@@ -21,32 +21,32 @@ export const Photos = () => {
             <div className="absolute inset-0 bg-black/60 pointer-events-none z-10"></div>
             <div className="col-span-2 row-span-2 h-150 xl:h-200 overflow-hidden w-full">
                 <Text t="Freshly baked cookies you can't miss in Manhattan" cName="md:text-[28px] xl:text-[42px] xl:mt-165 md:mt-110 max-w-[350px] xl:max-w-[550px] 2xl:max-w-[650px]" />
-                <Slika src="/home2-suites-concept/assets/PhotosSection/cookiesC.jpg" cName="object-[50%]" />
+                <Slika src="/home2-suites-concept/assets/PhotosSection/cookiesC.jpg" cName="object-[50%]" altText="Image of a Cookie" />
             </div>
 
             <Z cols="1" rows="1" h="100" cName="">
                 <Text t="A green oasis in the heart of Manhattan to explore" cName="xl:mt-65 2xl:mt-75 md:mt-45 md:text-[16px] xl:max-w-[280px] xl:text-[28px] 2xl:max-w-[340px] md:max-w-[180px] md:ml-2 lg:mt-50 lg:max-w-[220px]" />
-                <Slika src="/home2-suites-concept/assets/PhotosSection/centralParkC.jpg" cName="" />
+                <Slika src="/home2-suites-concept/assets/PhotosSection/centralParkC.jpg" cName="" altText="Image of Central Park" />
             </Z>
 
             <Z cols="1" rows="1" h="100" cName="">
                 <Text t="Unmissable shows lighting up Broadway's legendary stages" cName="xl:mt-55 2xl:mt-65 md:mt-45 md:text-[16px] xl:max-w-[280px] xl:text-[28px] 2xl:max-w-[340px] md:max-w-[180px] md:ml-2 lg:mt-50 lg:max-w-[220px]" />
-                <Slika src="/home2-suites-concept/assets/PhotosSection/broadwayC.png" cName="" />
+                <Slika src="/home2-suites-concept/assets/PhotosSection/broadwayC.png" cName="" altText="Image of Broadway" />
             </Z>
 
             <Z cols="2" rows="2" h="200" cName="">
                 <Text t="Iconic skyline views you'll never forget in Manhattan" cName="xl:mt-165 md:mt-110 md:text-[28px] xl:text-[42px]" />
-                <Slika src="/home2-suites-concept/assets/PhotosSection/landscapeC.jpg" cName="object-[73%]" />
+                <Slika src="/home2-suites-concept/assets/PhotosSection/landscapeC.jpg" cName="object-[73%]" altText="Image showing the stunning view of New York City's skyline" />
             </Z>
 
             <Z cols="1" rows="1" h="100" cName="">
                 <Text t="The world-famous symbol of freedom you must see" cName="xl:mt-65 2xl:mt-75 md:mt-45 md:text-[16px] xl:max-w-[280px] xl:text-[28px] 2xl:max-w-[340px] md:max-w-[180px] md:ml-2 lg:mt-50 lg:max-w-[220px]" />
-                <Slika src="/home2-suites-concept/assets/PhotosSection/libertyC.jpg" cName="object-[30%]" />
+                <Slika src="/home2-suites-concept/assets/PhotosSection/libertyC.jpg" cName="object-[30%]" altText="Image showing stunning view of Statue of Liberty City" />
             </Z>
             
             <Z cols="1" rows="1" h="100" cName="">
                 <Text t="Picture-perfect views and charm to discover in DUMBO" cName="2xl:mt-65 xl:mt-65 md:mt-45 md:text-[16px] xl:max-w-[280px] xl:text-[28px] 2xl:max-w-[330px] md:max-w-[180px] md:ml-2 lg:mt-50 lg:max-w-[220px]" />
-                <Slika src="/home2-suites-concept/assets/PhotosSection/dumboC.jpg" cName="" />
+                <Slika src="/home2-suites-concept/assets/PhotosSection/dumboC.jpg" cName="" altText="Image showing the view of Manhattan Bridge from Dumbo" />
             </Z>
             
         </section>
